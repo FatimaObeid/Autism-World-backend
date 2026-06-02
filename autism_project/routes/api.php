95ec6f3b8626d0a67b8a710d77963fd676024681
FatimaObeid <101230063@mu.edu.lb>
2026-06-02 22:04:04 +0300

    Update api.php

--- autism_project/routes/api.php
+++ autism_project/routes/api.php
@@ -5,10 +5,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\SpecialistController;
+use App\Http\Controllers\API\SpecialistController;
+use App\Http\Controllers\API\RegisterController;
 
 Route::post('/login', [AuthenticatedSessionController::class, 'mobileLogin'])->name('mobile.login');
 Route::post('/logout', [AuthenticatedSessionController::class, 'logout'])->name('mobile.logout');
+Route::post('/register', [RegisterController::class, 'register']);
 Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::prefix('specialist')->group(function () {
