b29f91ec7b9f6cdf566fd31a4a1adbfe95c5fc69
101230308 <101230308@mu.edu.lb>
2026-06-30 23:47:54 +0300

    Add date and target audience fields to Workshop model

--- autism_project/app/Models/Workshop.php
+++ autism_project/app/Models/Workshop.php
@@ -12,11 +12,32 @@ class Workshop extends Model
         'location',
         'status',
         'volunteer_id',
-        'workshop_time'
+        'workshop_time',
+        'date',
+        'target_audience',
+        'status',
     ];
-
+    protected $casts = [
+        'date' => 'date',
+        'workshop_time' => 'datetime:H:i',
+    ];
     public function volunteer()
     {
         return $this->belongsTo(Volunteer::class);
+    }
+
+    public function specialists()
+    {
+        return $this->belongsToMany(
+            Specialist::class,
+            'specialist_workshop',
+            'workshop_id',
+            'specialist_id'
+        )->withTimestamps();
+    }
+
+    public function isSpecialistRegistered($specialistId)
+    {
+        return $this->specialists()->where('specialist_id', $specialistId)->exists();
     }
 }
